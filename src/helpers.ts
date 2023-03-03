0a7c5d2b0dc43dde040939844a79b67023d01f60
lukas <akashare-creationgraphics@outlook.com>
2023-03-03 12:25:08 -0300

    first commit

---
+++ src/helpers.ts
@@ -0,0 +1,12 @@
+import { BinaryLike, createHash } from 'crypto'
+
+
+export function hash (data: BinaryLike) {
+    return createHash('sha256').update(data).digest('hex')
+}
+
+export function isHashProofed ({ hash, difficulty = 2, prefix = '0'}: {hash: string,
+difficulty?: number, prefix?: string}) {
+    const check = prefix.repeat(difficulty)
+    return hash.startsWith(check)
+}
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {Blockchain} from './blockchain'
+
+const blockchain = new Blockchain(Number(process.argv[2] || 4))
+const blockNumber = +process.argv[3] || 10
+
+let chain = blockchain.chain
+
+for (let i = 1; i < blockNumber; i++) {
+    const block = blockchain.createBlock(`Block ${i}`)
+    const mineInfo = blockchain.mineBlock(block)
+    chain = blockchain.pushBlock(mineInfo.minedBlock)
+}
+
+console.log('---- BLOCKCHAIN -----')
+console.log(chain)
